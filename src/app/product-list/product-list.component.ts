d48a8fae9169817e891406d26f6d27f62686e80d
Lostinrave <benaskalvinas@gmail.com>
2021-10-16 15:00:38 +0300

    exercises

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -5,16 +5,31 @@ import { products } from '../products';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
   products = products;
 
-  share() {
-    window.alert('The product has been shared!');
+  shareFacebook() {
+    window.alert('Share to facebook');
+
+    // Cia galetu buti modalinis langas pasirkinti kaip sharinti: facebook, twitter, email
+
+    // Facebook Sharer URL dokumentacija, kokia turi buti nuoroda:
+    // https://developers.facebook.com/docs/sharing/reference/share-dialog/
+    let url =
+      'https://facebook.com/sharer.php?display=popup&u=' + window.location.href;
+    window.open(url, 'sharer');
+  }
+
+  shareTwitter() {
+    window.alert('Share to twitter');
+    let url =
+      'https://twitter.com/intent/tweet?text=' +
+      window.location.href;
+    window.open(url, 'tShare' );
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
--- src/app/products.ts
+++ src/app/products.ts
@@ -8,21 +8,24 @@ export interface Product {
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
+    name: 'Iphone 13',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    photo: 'https://images.kaina24.lt/43/63/iphone-13-128gb.jpg'
   },
   {
     id: 2,
-    name: 'Phone Mini',
+    name: 'Samsung s21',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    photo:'https://images.kaina24.lt/43/14/samsung-galaxy-s21-128gb-2.jpg'
   },
   {
     id: 3,
-    name: 'Phone Standard',
+    name: 'One plus 9',
     price: 299,
-    description: ''
+    description: '',
+    photo:'https://images.kaina24.lt/43/93/oneplus-9-pro-2.jpg'
   }
 ];
 
